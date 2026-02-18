985bf2dd26d1ae54c3fd99ea31a980a023d66c47
Karine <karinegasesyan@gmail.com>
2026-02-18 18:22:48 +0400

    Feat: get todos with userId 1

--- tests/todos.spec.ts
+++ tests/todos.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe.only("JSON Placeholder Todos API", () => {
+test.describe("JSON Placeholder Todos API", () => {
   test("Get request for todos", async ({ request }) => {
     const response = await request.get(
       "https://jsonplaceholder.typicode.com/todos",
@@ -19,20 +19,21 @@ test.describe.only("JSON Placeholder Todos API", () => {
     }
   });
 
-  // test("GET /todos/1 - returns todo with id 1", async ({ request }) => {
-  //   const response = await request.get(
-  //     "https://jsonplaceholder.typicode.com/todos?userId=1",
-  //   );
+  test("GET /todos/1 - returns todo with id 1", async ({ request }) => {
+    const response = await request.get(
+      "https://jsonplaceholder.typicode.com/todos?userId=1",
+    );
 
-  //   expect(response.status()).toBe(200);
+    expect(response.status()).toBe(200);
 
-  //   const todo = await response.json();
-
-  //   expect(todo).toHaveProperty("userId", 1);
-  //   expect(todo).toHaveProperty("id");
-  //   expect(todo).toHaveProperty("title");
-  //   expect(todo).toHaveProperty("completed");
-  // });
+    const todos = await response.json();
+    for (let todo of todos) {
+      expect(todo).toHaveProperty("userId", 1);
+      expect(todo).toHaveProperty("id");
+      expect(todo).toHaveProperty("title");
+      expect(todo).toHaveProperty("completed");
+    }
+  });
 
   test("GET /todos/9999 - returns empty object", async ({ request }) => {
     const response = await request.get(
